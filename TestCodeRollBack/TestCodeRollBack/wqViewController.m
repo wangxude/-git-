c2fd208b072dca6136b391a511aa218c83ddff04
WANGXU7 <wangxu7@kingsoft.com>
2018-02-05 11:33:38 +0800

    测试1

--- TestCodeRollBack/TestCodeRollBack/wqViewController.m
+++ TestCodeRollBack/TestCodeRollBack/wqViewController.m
@@ -20,6 +20,10 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)test1 {
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
